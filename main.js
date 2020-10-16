0b9b0ced5e535c47e0c80354999729c8162cad67
DSLEGEND2403 <71708093+DSLEGEND2403@users.noreply.github.com>
2020-10-16 18:13:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload()
+{}
+
+function setup()
+{
+canvas= createCanvas(640,480);
+canvas.position(350,350);
+
+}
+
+function draw()
+{
+
+rect(600, 57, 55, 55);
+rect(10, 57, 55, 55);
+rect(102, 57, 55, 55);
+rect(198, 57, 55, 55);
+rect(119, 57, 55, 55);
+circle(600, 100, 125);
+circle(600, 200, 125);
+circle(600, 300, 125);
+circle(600, 133, 125);
+circle(600, 503, 125);
+circle(600, 206, 125);
+circle(600, 255, 125);
+circle(600, 20, 125);
+triangle(298, 75, 58, 20, 86, 75);
+triangle(73, 75, 58, 20, 86, 75);
+triangle(100, 75, 58, 20, 86, 75);
+triangle(40, 75, 58, 20, 86, 75);
+triangle(106, 75, 58, 20, 86, 75);
+triangle(236, 75, 58, 20, 86, 75);
+triangle(52, 75, 58, 20, 86, 201);
+triangle(52, 75, 58, 20, 20, 100);
+print(hex(255));
+curve(5, 26, 73, 24, 73, 61, 15, 65);
+stroke(255, 102, 0);
+fill('red')
+
+}
